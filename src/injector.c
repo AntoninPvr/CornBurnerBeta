9bafe9daf2196df30a91b4322ddce04fb434ceeb
Antonin <pivard.antonin@gmail.com>
2024-01-12 15:00:24 +0100

    change HAL to __HAL

--- src/injector.c
+++ src/injector.c
@@ -12,8 +12,8 @@ void HAL_GPTIO_EXTI_Callback(uint16_t GPIO_Pin) {
         if (HAL_GPIO_ReadPin(INJ_IN_0_GPIO_Port, INJ_IN_0_Pin) == GPIO_PIN_SET) {
             injector_state[0] = 1;
             HAL_GPIO_WritePin(INJ_OUT_0_GPIO_Port, INJ_OUT_0_Pin, GPIO_PIN_SET);
-            __HAL_TIM_Base_Start(&htim3);
-            __HAL_TIM_RESET_COUNTER(&htim3);
+            HAL_TIM_Base_Start(&htim3);
+            __HAL_TIM_SET_COUNTER(&htim3, 0);
         }
         else {
             inj_time[0] = __HAL_TIM_GET_COUNTER(&htim3);
@@ -24,8 +24,8 @@ void HAL_GPTIO_EXTI_Callback(uint16_t GPIO_Pin) {
         if (HAL_GPIO_ReadPin(INJ_IN_1_GPIO_Port, INJ_IN_1_Pin) == GPIO_PIN_SET) {
             injector_state[1] = 1;
             HAL_GPIO_WritePin(INJ_OUT_1_GPIO_Port, INJ_OUT_1_Pin, GPIO_PIN_SET);
-            __HAL_TIM_Base_Start(&htim4);
-            __HAL_TIM_RESET_COUNTER(&htim4);
+            HAL_TIM_Base_Start(&htim4);
+            __HAL_TIM_SET_COUNTER(&htim4, 0);
         }
         else {
             inj_time[1] = __HAL_TIM_GET_COUNTER(&htim4);
@@ -36,8 +36,8 @@ void HAL_GPTIO_EXTI_Callback(uint16_t GPIO_Pin) {
         if (HAL_GPIO_ReadPin(INJ_IN_2_GPIO_Port, INJ_IN_2_Pin) == GPIO_PIN_SET) {
             injector_state[2] = 1;
             HAL_GPIO_WritePin(INJ_OUT_2_GPIO_Port, INJ_OUT_2_Pin, GPIO_PIN_SET);
-            __HAL_TIM_Base_Start(&htim4);
-            __HAL_TIM_RESET_COUNTER(&htim4);
+            HAL_TIM_Base_Start(&htim4);
+            __HAL_TIM_SET_COUNTER(&htim4, 0);
         }
         else {
             inj_time[2] = __HAL_TIM_GET_COUNTER(&htim4);
@@ -48,8 +48,8 @@ void HAL_GPTIO_EXTI_Callback(uint16_t GPIO_Pin) {
         if (HAL_GPIO_ReadPin(INJ_IN_3_GPIO_Port, INJ_IN_3_Pin) == GPIO_PIN_SET) {
             injector_state[3] = 1;
             HAL_GPIO_WritePin(INJ_OUT_3_GPIO_Port, INJ_OUT_3_Pin, GPIO_PIN_SET);
-            __HAL_TIM_Base_Start(&htim3);
-            __HAL_TIM_RESET_COUNTER(&htim3);
+            HAL_TIM_Base_Start(&htim3);
+            __HAL_TIM_SET_COUNTER(&htim3, 0);
         }
         else {
             inj_time[3] = __HAL_TIM_GET_COUNTER(&htim3);
