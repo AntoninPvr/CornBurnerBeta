05ff4d594a1a72cbf508e4a30fd477bdf6642a34
Antonin <pivard.antonin@gmail.com>
2024-01-13 00:12:55 +0100

    add dma init

--- lib/cubemx/src/driver.c
+++ lib/cubemx/src/driver.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "driver.h"
 #include "adc.h"
+#include "dma.h"
 #include "i2c.h"
 #include "rtc.h"
 #include "tim.h"
@@ -90,6 +91,7 @@ int driver(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_ADC1_Init();
   MX_I2C1_Init();
   MX_RTC_Init();
